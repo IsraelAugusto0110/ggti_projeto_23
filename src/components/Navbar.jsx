1acea2e772010a7daca62ad5479093f62d3659fa
IsraelAugusto0110 <israelaugustofurtadosantos@gmail.com>
2023-03-06 19:46:08 -0300

    Projeto iniciado. Criado o menu de navegação com as rotas

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,52 @@
-import { AppBar, Typography } from "@mui/material";
+import { Explore } from "@mui/icons-material";
+import {
+  AppBar,
+  Button,
+  Grid,
+  Stack,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
 
+const linkStyle = {
+  textDecoration: "none",
+  color: "white",
+};
+
 const Navbar = () => {
   return (
-    <AppBar position="static">
-      <Typography>GGTI 2023</Typography>
-      <Link to="/">Home</Link>
-      <Link to="/destinos">Destinos</Link>
-    </AppBar>
+    <>
+      <AppBar
+        position="static"
+        sx={{ backgroundColor: "black", opacity: 0.25 }}
+      >
+        <Toolbar>
+          <Grid container>
+            <Grid item xs={2}>
+              <Typography>
+                <Explore />
+              </Typography>
+            </Grid>
+            <Grid item xs={5}>
+              <Stack direction="row">
+                <Button>
+                  <Link to="/" style={linkStyle}>
+                    HOME
+                  </Link>
+                </Button>
+                <Button>
+                  <Link to="/destinos" style={linkStyle}>
+                    DESTINOS
+                  </Link>
+                </Button>
+              </Stack>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </>
   );
 };
 
