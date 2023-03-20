7015d12ae2c664595c2dd7e2ec7c5f27aaeb8965
IsraelAugusto0110 <israelaugustofurtadosantos@gmail.com>
2023-03-20 19:38:04 -0300

    tela finalizada

--- src/components/Destino.jsx
+++ src/components/Destino.jsx
@@ -14,6 +14,33 @@ const linkStyle = {
   textDecoration: "none",
 };
 
+const locations = [
+  {
+    id: 1,
+    nome: "Ilha de Creta - Grécia",
+    imageUrl: require("../assets/images/crete01.jpg"),
+    title: "Island of Crete - Greece",
+    locDesc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut magna in nulla pretium volutpat eu eu turpis.",
+  },
+  {
+    id: 2,
+    nome: "Athenas - Grécia",
+    imageUrl: require("../assets/images/athens01.jpg"),
+    title: "Athens - Greece",
+    locDesc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut magna in nulla pretium volutpat eu eu turpis.",
+  },
+  {
+    id: 3,
+    nome: "Florença - Italia",
+    imageUrl: require("../assets/images/florence01.jpg"),
+    title: "Florence - Italy",
+    locDesc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut magna in nulla pretium volutpat eu eu turpis.",
+  },
+];
+
 function Destino() {
   return (
     <>
@@ -25,34 +52,38 @@ function Destino() {
           justifyContent: "space-evenly",
         }}
       >
-        <Card sx={{ maxWidth: 345, height: 500, marginTop: 2 }}>
-          <CardMedia
-            sx={{ height: 250 }}
-            image={require("../assets/images/crete01.jpg")}
-            title="Island of Crete - Greece"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Ilha de Creta - Grécia
-            </Typography>
-            <Typography gutterBottom variant="body1" component="div">
-              A partir de 10 X $199,90
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut
-              magna in nulla pretium volutpat eu eu turpis.
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="medium" variant="text">
-              <Link to="/detalhe" style={linkStyle}>
-                <Typography variant="body2" fontWeight="bold" color="skyblue">
-                  Saiba mais
-                </Typography>
-              </Link>
-            </Button>
-          </CardActions>
-        </Card>
+        {locations.map((location) => (
+          <Card
+            key={location.id}
+            sx={{ maxWidth: 345, height: 500, marginTop: 2 }}
+          >
+            <CardMedia
+              sx={{ height: 250 }}
+              image={location.imageUrl}
+              title={location.title}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {location.nome}
+              </Typography>
+              <Typography gutterBottom variant="body1" component="div">
+                A partir de 10 X $199,90
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {location.locDesc}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="medium" variant="text">
+                <Link to="/detalhe" style={linkStyle}>
+                  <Typography variant="body2" fontWeight="bold" color="skyblue">
+                    Saiba mais
+                  </Typography>
+                </Link>
+              </Button>
+            </CardActions>
+          </Card>
+        ))}
       </Grid>
     </>
   );
