580d1cfaa226182e8819b0a231a7645d3b7c328c
rafaeldossper <rafahsanttos96@gmail.com>
2023-03-22 10:04:43 -0300

    Tela detalhes do destino - comecei a estruturar a tela

--- src/components/DetalheDestino.jsx
+++ src/components/DetalheDestino.jsx
@@ -1,7 +1,65 @@
+import { Grid, Typography } from "@mui/material";
 import React from "react";
 
 function DetalheDestino() {
-  return <div>DetalheDestino</div>;
+  return (
+    <>
+      <Grid
+        container
+        sx={{
+          backgroundColor: "red",
+          padding: 2,
+          marginTop: 2,
+          justifyContent: "center",
+        }}
+        flexDirection="row"
+      >
+        <Grid
+          container
+          item
+          sx={{
+            backgroundColor: "royalblue",
+            maxHeighteight: 500,
+            minHeight: 250,
+            width: "30%",
+            padding: 2,
+          }}
+        ></Grid>
+        <Grid
+          container
+          item
+          sx={{
+            backgroundColor: "purple",
+            maxHeighteight: 500,
+            minHeight: 250,
+            width: "30%",
+            padding: 2,
+          }}
+        >
+          <Grid
+            item
+            sx={{ backgroundColor: "white", textAlign: "justify", padding: 1 }}
+          >
+            <Typography variant="h5" gutterBottom fontWeight="bold">
+              Atenas - Grécia
+            </Typography>
+            <Typography variant="h6" gutterBottom fontWeight="bold">
+              Sobre
+            </Typography>
+            <Typography variant="p" fontSize={20} textAlign="justify">
+              Lorem ipsum dolor sit amet. Sed iste minima quo deserunt inventore
+              aut deserunt dolores cum minima omnis sit praesentium totam nam
+              rerum doloribus. Est molestiae debitis ut facilis eveniet et
+              dignissimos adipisci est voluptatibus totam ut cupiditate iste! Et
+              sint quia vel quam error est deleniti natus. Eos nostrum corporis
+              sit vero ipsa ea omnis galisum id fugiat necessitatibus aut velit
+              dolorem ut asperiores assumenda.
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
+  );
 }
 
 export default DetalheDestino;
